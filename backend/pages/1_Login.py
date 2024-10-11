7f5af43eff884e745b0e16a156bdf70a2ef8bc29
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 02:35:00 +0700

    Update 1_Login.py

--- backend/pages/1_Login.py
+++ backend/pages/1_Login.py
@@ -27,10 +27,13 @@ def login():
         try:
             user = auth.sign_in_with_email_and_password(email, password)
             st.success("Login สำเร็จ!")
-            st.session_state.current_page = "home"  # เปลี่ยนไปยังหน้า Home
-            st.switch_page("5_Home")  # สลับไปยังหน้า Home
+            
         except:
             st.error("Login ไม่สำเร็จ กรุณาตรวจสอบข้อมูลอีกครั้ง.")
+
+    if st.session_state.login_status == "success":
+        st.session_state.current_page = "home"  # เปลี่ยนไปยังหน้า Home
+        st.switch_page("5_Home")  # สลับไปยังหน้า Home
 
 if __name__ == "__main__":
     login()
