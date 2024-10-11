34f189775d0072866e3ee4a4c15af0bc547b7b55
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-11 20:57:04 +0700

    Update home.py

--- backend/home.py
+++ backend/home.py
@@ -60,10 +60,12 @@ def login():
                 if st.button("Sign Up"):  # แสดงปุ่ม Sign Up
                     st.session_state.show_sign_up = True  # เปลี่ยนสถานะเป็นต้องการลงทะเบียน
                     st.experimental_rerun()  # เริ่มต้นการทำงานใหม่
-if st.session_state.get('show_sign_up', False):
-    sign_up()  # เรียกฟังก์ชันแสดงฟอร์มลงทะเบียน
-    if st.button("Sign Up"):
-        st.session_state.show_sign_up = True  # แสดงฟอร์มลงทะเบียน
+    if st.session_state.get('show_sign_up', False):
+        sign_up()  # เรียกฟังก์ชันแสดงฟอร์มลงทะเบียน
+    else:
+        login()  # แสดงหน้า login ถ้ายังไม่ได้แสดงฟอร์ม Sign Up
+        if st.button("Sign Up"):
+            st.session_state.show_sign_up = True  # แสดงฟอร์มลงทะเบียน
 
 # ฟังก์ชันสำหรับหน้า Sign Up
 def sign_up():
