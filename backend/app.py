ce5f05824f00554e13b12f60b87d18b579a9e1cf
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-11 15:26:23 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,18 +3,18 @@ import os
 
 # ฟังก์ชันหลัก
 def main():
-    st.sidebar.title("เมนู")
-    page = st.sidebar.radio("เลือกหน้า:", ["หน้าแรก", "หน้าอัปโหลด", "Chatbot"])
+    # st.sidebar.title("เมนู")
+    # page = st.sidebar.radio("เลือกหน้า:", ["หน้าแรก", "หน้าอัปโหลด", "Chatbot"])
 
-    if page == "หน้าแรก":
-        from pages.home import show_home  # นำเข้าฟังก์ชันจาก home.py
-        show_home()
-    elif page == "หน้าอัปโหลด":
-        from pages.upload import show_upload  # นำเข้าฟังก์ชันจาก upload.py
-        show_upload()
-    elif page == "Chatbot":
-        from pages.chat import show_chat  # นำเข้าฟังก์ชันจาก chat.py
-        show_chat()
+    # if page == "หน้าแรก":
+    #     from pages.home import show_home  # นำเข้าฟังก์ชันจาก home.py
+    #     show_home()
+    # elif page == "หน้าอัปโหลด":
+    #     from pages.upload import show_upload  # นำเข้าฟังก์ชันจาก upload.py
+    #     show_upload()
+    # elif page == "Chatbot":
+    #     from pages.chat import show_chat  # นำเข้าฟังก์ชันจาก chat.py
+    #     show_chat()
 
 if __name__ == "__main__":
     main()
