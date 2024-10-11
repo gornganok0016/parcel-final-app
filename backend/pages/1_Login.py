c749de6b507ed9448f3535530161e0597bbecb42
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 02:12:28 +0700

    Update 1_Login.py

--- backend/pages/1_Login.py
+++ backend/pages/1_Login.py
@@ -1,9 +1,6 @@
 import streamlit as st
-import os
-import pandas as pd
-from model import read_name_from_image, crop_and_read_names, save_to_csv, count_names_in_csv
 import pyrebase
-from streamlit_chat import message
+
 
 # Firebase config
 firebaseConfig = {
