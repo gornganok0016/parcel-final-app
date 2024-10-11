c221eb08e416c172ea8fbc92e6996834ecc0c61b
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 00:50:19 +0700

    Update Admin.py

--- backend/pages/Admin.py
+++ backend/pages/Admin.py
@@ -22,25 +22,25 @@ bot_avatar = "https://firebasestorage.googleapis.com/v0/b/posto-ai-app.appspot.c
 
 
 st.title("หน้าอัปโหลด")
-    uploaded_file = st.file_uploader("อัปโหลดไฟล์", type=['jpg', 'png'])
+uploaded_file = st.file_uploader("อัปโหลดไฟล์", type=['jpg', 'png'])
     
-    if uploaded_file is not None:
-        image_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
+if uploaded_file is not None:
+    image_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
         
         # บันทึกไฟล์ภาพ
-        with open(image_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
+    with open(image_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
         
         # ตรวจจับชื่อจากภาพ
-        detected_names, boxes = read_name_from_image(image_path)
+    detected_names, boxes = read_name_from_image(image_path)
 
-        if detected_names:
-            cropped_names = crop_and_read_names(image_path, boxes)
-            save_to_csv(cropped_names)
+    if detected_names:
+        cropped_names = crop_and_read_names(image_path, boxes)
+        save_to_csv(cropped_names)
 
             # แสดงผลลัพธ์
-            st.write("Detected Names: ", ", ".join(detected_names))
-            st.write("Cropped Names: ", ", ".join(cropped_names))
-            st.write("Name Counts: ", count_names_in_csv().to_dict(orient='records'))
-        else:
-            st.warning("ไม่พบชื่อในภาพ")
+        st.write("Detected Names: ", ", ".join(detected_names))
+        st.write("Cropped Names: ", ", ".join(cropped_names))
+        st.write("Name Counts: ", count_names_in_csv().to_dict(orient='records'))
+    else:
+        st.warning("ไม่พบชื่อในภาพ")
