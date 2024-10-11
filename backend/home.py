a5ae61f07194d7a4253fe057f016f7dc80a2969f
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 01:18:01 +0700

    Update home.py

--- backend/home.py
+++ backend/home.py
@@ -10,6 +10,10 @@ def main():
         st.switch_page("Login")  # ไปที่หน้า Login
     elif st.session_state.current_page == "sign_up":
         st.switch_page("SignUp")  # ไปที่หน้า Sign Up
+    elif st.session_state.current_page == "chat":
+        st.switch_page("Chatbot")  # ไปที่หน้า Sign Up
+    elif st.session_state.current_page == "admin":
+        st.switch_page("Admin")  # ไปที่หน้า Sign Up
 
 if __name__ == "__main__":
     main()
