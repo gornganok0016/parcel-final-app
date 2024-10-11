eb93069a636d4d7718901629db6e4f8ea0d13ab3
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 00:19:34 +0700

    Create SignUp.py

---
+++ backend/SignUp.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import os
+import pandas as pd
+from model import read_name_from_image, crop_and_read_names, save_to_csv, count_names_in_csv
+import pyrebase
+from streamlit_chat import message
+
+# Firebase config
+firebaseConfig = {
+    'apiKey': "AIzaSyCt7JaHwmHCS9Lm_hiZQv1B2XM_1eR4zPM",
+    'authDomain': "posto-ai-app.firebaseapp.com",
+    'databaseURL': "https://YOUR_PROJECT_ID.firebaseio.com",
+    'projectId': "posto-ai-app",
+    'storageBucket': "posto-ai-app.appspot.com",
+    'messagingSenderId': "408360408985",
+    'appId': "1:408360408985:web:55ec7842c40203f28c6508",
+    'measurementId': "G-HL46XMRBKM"
+}
+
+# Firebase initialization
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+st.title("Sign Up")
+  email = st.text_input("Email")
+  password = st.text_input("Password", type="password")
+      
+if st.button("Sign Up"):
+    st.success("Sign Up สำเร็จ!")
+    st.switch_page("Login")  # กลับไปที่หน้า Login
