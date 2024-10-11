67b4cc697ba61ed63c457813dde71cf8075728d9
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 01:09:35 +0700

    Update home.py

--- backend/home.py
+++ backend/home.py
@@ -9,8 +9,6 @@ if page == "Login":
     st.switch_page("Login")  # สลับไปที่หน้า Login
 elif page == "Sign Up":
     st.switch_page("SignUp")  # สลับไปที่หน้า Sign Up
-elif page == "หน้าแรก":
-    st.switch_page("Home")  # สลับไปที่หน้า Home
 elif page == "หน้าอัปโหลด":
     st.switch_page("Admin")  # สลับไปที่หน้า Admin
 elif page == "Chatbot":
