c0f5fa9769f8ece31223fba657f077dcea5f1193
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 14:31:56 +0700

    Update home.py

--- backend/home.py
+++ backend/home.py
@@ -8,13 +8,6 @@ if "current_page" not in st.session_state:
 if st.session_state.current_page == "login":
     ShowSidebarNavigation = false
     
-def check_login():
-    if "login_status" not in st.session_state:
-        st.session_state.login_status = False  # ตั้งค่าเริ่มต้นเป็น None
-    if not st.session_state.login_status:
-        st.warning("กรุณา Login ก่อนเข้าหน้าอื่น")
-        st.stop()  # หยุดการทำงานถ้าผู้ใช้ยังไม่ได้ล็อกอิน
-    
 def main():
     # ตรวจสอบสถานะของ current_page และเรียก switch_page ตามหน้า
    if st.session_state.current_page == "login":
