1d4263395d93a8cae1f6fd1406d009dd4da8869b
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 21:37:28 +0700

    Update 4_Chatbot.py

--- backend/pages/4_Chatbot.py
+++ backend/pages/4_Chatbot.py
@@ -10,7 +10,7 @@ def change_colors():
            background-color: #f0f0f0;  /* สีพื้นหลัง */
        }
        .st-emotion-cache-vl2fub{
-            background-color: #f0f0f0;
+            color: #f0f0f0;
         }    
         .body{
             padding:0;
