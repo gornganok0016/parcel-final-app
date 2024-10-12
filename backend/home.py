f48009fe75a0a8ca035ef85a174ffe94ba1fc4a2
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 15:23:15 +0700

    Update home.py

--- backend/home.py
+++ backend/home.py
@@ -7,8 +7,35 @@ if "current_page" not in st.session_state:
 # ตรวจสอบว่าเป็นหน้า login หรือไม่
 if st.session_state.current_page == "login":
     ShowSidebarNavigation = false
+
+def change_colors():
+    style = """
+        <style>
+            body {
+                background-color: #333366;  /* สีพื้นหลัง */
+            }
+            .stText {
+                color: #2c3e50;  /* สีของตัวอักษร */
+            }
+            .stButton>button {
+                background-color: #3498db;  /* สีพื้นหลังของปุ่ม */
+                color: white;  /* สีของตัวอักษรในปุ่ม */
+            }
+            .stButton>button:hover {
+                background-color: #2980b9;  /* สีของปุ่มเมื่อชี้เมาส์ */
+            }
+            footer {
+                visibility: hidden;  /* ซ่อนฟุตเตอร์ */
+            }
+            #MainMenu {
+                visibility: hidden;  /* ซ่อนเมนูหลัก */
+            }
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
     
 def main():
+    change_colors()  # เรียกใช้ฟังก์ชันเพื่อเปลี่ยนสี
     # ตรวจสอบสถานะของ current_page และเรียก switch_page ตามหน้า
    if st.session_state.current_page == "login":
         st.switch_page("pages/1_Login.py")  # ไปที่หน้า Login
