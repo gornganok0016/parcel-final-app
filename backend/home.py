6ff3edd265133ce4aca42275c659f52091bee4b9
gornganok0016 <154305398+gornganok0016@users.noreply.github.com>
2024-10-12 22:51:43 +0700

    Update home.py

--- backend/home.py
+++ backend/home.py
@@ -13,13 +13,13 @@ def main():
     # ตรวจสอบสถานะของ current_page และเรียก switch_page ตามหน้า
    if st.session_state.current_page == "login":
         st.switch_page("pages/1_Login.py")  # ไปที่หน้า Login
-    elif st.session_state.current_page == "sign_up":
+   elif st.session_state.current_page == "sign_up":
         st.switch_page("pages/2_SignUp.py")  # ไปที่หน้า Sign Up
-    elif st.session_state.current_page == "home":
-        st.switch_page("pages/5_Home.py")  # ไปที่หน้า Home
-    elif st.session_state.current_page == "chat":
+   elif st.session_state.current_page == "home":
+        st.switch_page("pages/5_Home.py")  # ไปที่หน้า Home 
+   elif st.session_state.current_page == "chat":
         st.switch_page("pages/4_Chatbot.py")  # ไปที่หน้า Chatbot
-    elif st.session_state.current_page == "admin":
+   elif st.session_state.current_page == "admin":
         st.switch_page("pages/3_Admin.py")  # ไปที่หน้า Admin
 
 if __name__ == "__main__":
